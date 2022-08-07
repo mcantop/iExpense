0f100a85e1e4ddb5fe8c769ba763f0572b67e38e
mcantop <maciejcantop@icloud.com>
2022-08-07 13:07:59 +0200

    first commit

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,9 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    if item.type == "Personal" {
+                        Section {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(item.name)
+                                        .font(.headline)
+                                    Text(item.type)
+                                }
+                                
+                                Spacer()
+                                
+                                Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                                    .foregroundColor(
+                                        item.amount >= 10 ? (item.amount >= 100 ? .purple : .green) : .black
+                                    )
+                            }
+                        } header: {
+                            Text("Personal")
+                        }
+                    } else {
+                        Section {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(item.name)
+                                        .font(.headline)
+                                    Text(item.type)
+                                }
+                                
+                                Spacer()
+                                
+                                Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                                    .foregroundColor(
+                                        item.amount >= 10 ? (item.amount >= 100 ? .purple : .green) : .black
+                                    )
+                            }
+                        } header: {
+                            Text("Business")
+                        }
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense \(expenses.items.count == 0 ? "" : "(\(expenses.items.count))" )")
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        showingAddExpense = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                        .disabled(expenses.items.isEmpty)
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
